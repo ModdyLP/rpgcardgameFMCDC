1910f4d86f26c645053ee43370b1946c634264e7
preddath <jrheim267@gmail.com>
2017-08-11 10:53:53 +0200

    Joshua-added Ids

--- src/controller/HubController.java
+++ src/controller/HubController.java
@@ -1,12 +1,40 @@
 package controller;
 
 import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 
 public class HubController {
 
+    @FXML
+    private Pane hand1;
 
-    
-    public void setStatus(String status) {
+    @FXML
+    private Pane hand2;
+
+    @FXML
+    private Pane hand3;
+
+    @FXML
+    private Pane hand4;
+
+    @FXML
+    private Pane hero1;
+
+    @FXML
+    private Pane hero2;
+
+    @FXML
+    private Pane draw1;
+
+    @FXML
+    private Pane draw2;
+
+    @FXML
+    private Pane grave;
+
+
+    /*public void setStatus(String status) {
         Platform.runLater(()-> this.status.setText(status));
-    }
+    }*/
 }
